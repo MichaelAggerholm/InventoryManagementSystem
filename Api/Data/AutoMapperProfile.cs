c73a1f9acb5cebd7bca9f6949ee4c92d29e2be74
Aggerholm <92aggerholm@gmail.com>
2023-03-20 20:24:20 +0100

    Tilføjet automapper og profile som mapper user til userdto

---
+++ Api/Data/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Api.Dto;
+using Api.Models;
+using AutoMapper;
+
+namespace Api.Data;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        // Mapper fra User til UserDto, og fra UserDto til User.
+        CreateMap<User, UserDto>();
+        CreateMap<UserDto, User>();
+    }
+}
