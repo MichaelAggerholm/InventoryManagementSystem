fd50d539f6acd65967cfd17b25d10168a85daa3c
Aggerholm <92aggerholm@gmail.com>
2023-03-20 21:51:22 +0100

    oprettet unit test projekt for blazor app

---
+++ BlazorAppTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BlazorAppTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
